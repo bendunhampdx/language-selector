ceab4b12fb4225d3ac8042961e0ddb385abf2ac3
bendunhampdx <bendunhampdx@gmail.com>
2021-06-04 11:39:00 -0700

    update logic in javascript tp include more possible choices

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,46 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    const A = $("input:radio[name=drink]:checked").val();
+    const drink = $("input:radio[name=drink]:checked").val();
 
-    const B = $("input:radio[name=cheetos]:checked").val();
+    const cheetos = $("input:radio[name=cheetos]:checked").val();
 
-    const C = $("input:radio[name=sex]:checked").val();
+    const sex = $("input:radio[name=sex]:checked").val();
 
-    const D = $("input:radio[name=aliens]:checked").val();
+    const aliens = $("input:radio[name=aliens]:checked").val();
 
-    const E = $("input:radio[name=limbs]:checked").val();
+    const limbs = $("input:radio[name=limbs]:checked").val();
 
 
    
 
-    if (A === 'coffee' && B === 'regular' && C === 'yes' && (D === 'yes' || D === 'no')) {
+    if (drink === 'coffee' && cheetos === 'regular' && sex === 'yes' && (aliens === 'yes' || aliens === 'no')) {
       $("#match").show();
       $(".person1").text("C#").show();
     } 
-    else if (A === 'tea' && B === 'flaming' && C === 'no' && (D === 'yes' || D === 'no')) {
+    else if (drink === 'coffee' && cheetos === 'flaming' && sex === 'no' && aliens === 'yes' && limbs === 'feet' ) {
       $("#match").show();
-      $(".person1").text("JavaScript!").show();
+      $(".person1").text("C#").show();
+    }
+    else if (drink === 'tea' && cheetos === 'flaming' && sex === 'no' && (aliens === 'yes' || aliens === 'no')) {
+      $("#match").show();
+      $(".person1").text("JavaScript").show();
+    }
+    else if (drink === 'coffee' && cheetos === 'flaming' && sex === 'yes' && aliens === 'yes' && limbs === 'feet') {
+      $("#match").show();
+      $(".person1").text("Ruby").show();
+    }
+    else if (drink === 'tea' && cheetos === 'regular' && sex === 'no' && aliens === 'no' && limbs === 'hands') {
+      $("#match").show();
+      $(".person1").text("Python").show();
+    }
+    else if (drink === 'tea' && cheetos === 'regular' && sex === 'yes' && aliens === 'no' && limbs === 'feet') {
+      $("#match").show();
+      $(".person1").text("Rust").show();
     }
     else {
       $("#match").show();
-      $(".person1").text("Sorry, keep trying").show();
+      $(".person1").text("Swift").show();
     }
   });
 });
